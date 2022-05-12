32aa07bc88b24fb3b781f8837309567ffe277353
MarliesR <marlies.rieder95@gmail.com>
2022-05-12 12:16:34 +0200

    git push

--- block_showtime.php
+++ block_showtime.php
@@ -4,6 +4,20 @@ class block_showtime extends block_base {
         $this->title = get_string('showtime', 'block_showtime');
     }
 
+    public function specialization() {
+        if (isset($this->config)) {
+            if (empty($this->config->title)) {
+                $this->title = get_string('defaulttitle', 'block_simplehtml');            
+            } else {
+                $this->title = $this->config->title;
+            }
+    
+            if (empty($this->config->text)) {
+                $this->config->text = get_string('defaulttext', 'block_simplehtml');
+            }    
+        }
+    }
+
     public function get_content() {
         if ($this->content !== null) {
           return $this->content;
@@ -15,6 +29,10 @@ class block_showtime extends block_base {
      
         return $this->content;
     }
+
+    public function instance_allow_multiple() {
+        return true;
+      }
     // The PHP tag and the curly bracket for the class definition 
     // will only be closed after there is another function added in the next section.
 }
